cf0f4ec28aa204c7616e55d148b2f3f982414966
DickensJuma <dickensjuma13@gmail.com>
2022-02-03 13:23:40 +0300

    *

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Dashboard from "../src/components/Dashboard/Dashboard";
